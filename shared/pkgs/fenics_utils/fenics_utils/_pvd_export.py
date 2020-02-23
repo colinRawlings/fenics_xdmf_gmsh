368568b237ac59cce48ceea520cbaaa8f4da42aa
colinRawlings <colin.d.rawlings@gmail.com>
2020-02-23 17:44:22 +0100

    add method to calculate the gradient ready for export to work around slow pipeline updates when paraview does this.

--- shared/pkgs/fenics_utils/fenics_utils/_pvd_export.py
+++ shared/pkgs/fenics_utils/fenics_utils/_pvd_export.py
@@ -7,6 +7,7 @@
 import os
 
 import dolfin as fn
+import ufl
 
 ###########################################################
 # Definitions
@@ -27,8 +28,10 @@ def detect_function_degree(u: fn.Function) -> int:
     return u.ufl_element().degree()
 
 
-def refine_and_project_to_cg1(u: fn.Function,
-                              max_refinements: int = 2) -> fn.Function:
+#############################################################################
+
+
+def refine_and_project_to_cg1(u: fn.Function, max_refinements: int = 2) -> fn.Function:
     """Project a scalar or vector function supported
     on higher order elements onto first order elements 
     defined on a suitably refined mesh.
@@ -65,6 +68,9 @@ def refine_and_project_to_cg1(u: fn.Function,
     return ur
 
 
+#############################################################################
+
+
 def save_function(u: fn.Function, filepath: str):
 
     assert os.path.splitext(filepath)[-1] == '.pvd', "Expected a paraview file"
@@ -75,3 +81,33 @@ def save_function(u: fn.Function, filepath: str):
 
     file = fn.File(fn.MPI.comm_world, filepath)
     file << ur
+
+
+#############################################################################
+
+
+def calculate_gradient(u: fn.Function) -> fn.Function:
+    """Compute the gradient for use during post processing
+    
+    Arguments:
+        u {fn.Function} -- [description]
+    
+    Returns:
+        fn.Function -- [description]
+    """
+
+    assert u.num_sub_spaces() == 0, "Function does not appear to be a scalar"
+
+    V = fn.VectorFunctionSpace(u.function_space().mesh(), "CG", 1)
+    i = ufl.Index()
+
+    u_H = fn.TrialFunction(V)
+    v_H = fn.TestFunction(V)
+
+    a = u_H[i] * v_H[i] * fn.dx  # type: ignore
+    L = fn.Dx(u, i) * v_H[i] * fn.dx  # type: ignore
+
+    u_H = fn.Function(V)
+    fn.solve(a == L, u_H, [])
+
+    return u_H
