8c56403a4e4ca3a0e06df60bac34580eede114cd
colinRawlings <colin.d.rawlings@gmail.com>
2020-02-23 17:44:45 +0100

    add tests for the gradient calculation

--- shared/pkgs/fenics_utils/tests/test_export.py
+++ shared/pkgs/fenics_utils/tests/test_export.py
@@ -71,3 +71,32 @@ def test_refinement_degree2(cg2_2d_scalar_func):
 
 def test_refinement_degree2(cg2_2d_vector_func):
     ur = fu.refine_and_project_to_cg1(cg2_2d_vector_func)
+
+def test_gradient_cg1_calc(mesh_3d):
+
+    V = fn.FunctionSpace(mesh_3d, "CG", 1)
+
+    u = fn.Function(V)
+    u.interpolate(fn.Expression("exp(-(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]))*sin(x[0])", degree=2))
+
+    H = fu.calculate_gradient(u)
+
+def test_gradient_cg2_calc(mesh_3d):
+
+    V = fn.FunctionSpace(mesh_3d, "CG", 1)
+
+    u = fn.Function(V)
+    u.interpolate(fn.Expression("exp(-(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]))*sin(x[0])", degree=2))
+
+    H = fu.calculate_gradient(u)
+
+def test_invalid_gradient_calc(mesh_3d):
+
+    W = fn.VectorFunctionSpace(mesh_3d, "CG", 1)
+
+    u = fn.Function(W)
+    u.interpolate(fn.Expression(("exp(-(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]))*sin(x[0])", "0", "1"), degree=2))
+
+    with pytest.raises(AssertionError):  # type: ignore
+        H = fu.calculate_gradient(u)
+        
